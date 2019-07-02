bab640886f6b686bc1d6539f2b5b7345cdcb49ed
Gediminas Šaltenis <gediminas@saltenis.lt>
2019-07-03 02:02:14 +0300

    WIP

--- include/main.h
+++ include/main.h
@@ -2,27 +2,33 @@
 #define main_h
 
 #include <Arduino.h>
+#include <Wire.h>
 #include <Adafruit_ST7735.h>
 #include <WeatherCalculations.h>
 #include <SparkFunBME280.h>
 #include <SparkFunCCS811.h>
 
-#define TFT_CS 10
-#define TFT_RST 8
-#define TFT_DC 9
+#define DISPLAY_CS 10
+#define DISPLAY_RST 8
+#define DISPLAY_DC 9
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+Adafruit_ST7735 display = Adafruit_ST7735(DISPLAY_CS, DISPLAY_DC, DISPLAY_RST);
 
+#define BME280_I2C 0x76
 BME280 sensorBME280;
 bool sensorBME280connected = false;
-
 float dataTemperature = 0.0F;
 float dataHumidity = 0.0F;
 float dataPressure = 0.0F;
 
+#define CCS811_I2C 0x5A
+CCS811 sensorCCS811(CCS811_I2C);
+bool sensorCCS811connected = false;
+
 void setup();
 void loop();
 void initBME280();
+void initCCS811();
 void readData();
 void printData();
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,140 +1,161 @@
 #include <main.h>
 
 void setup() {
-  tft.initR(INITR_BLACKTAB); 
-  tft.setRotation(3);
-  tft.fillScreen(ST7735_BLACK);
+    Wire.begin();
 
-  tft.setCursor(0, 0);
-  tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
-  tft.println();
-  tft.println("Loading");
-  tft.println();
+    display.initR(INITR_BLACKTAB);
+    display.setRotation(3);
+    display.fillScreen(ST7735_BLACK);
 
-  initBME280();
+    display.setCursor(0, 0);
+    display.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    display.println();
+    display.println("Loading");
+    display.println();
 
-  tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
-  tft.println();  
-  tft.println("Done");
-  tft.println();
-  
-  delay(1000);
-  if (sensorBME280connected) {
-    tft.fillScreen(ST7735_BLACK);  
-  }
+    initBME280();
+    display.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    initCCS811();
+
+    display.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    display.println();
+    display.println("Done");
+    display.println();
+
+    delay(1000);
+    if (sensorBME280connected || sensorCCS811connected) {
+        display.fillScreen(ST7735_BLACK);
+    }
 }
 
 void loop() {
-  readData();
-  printData();
-  delay(1000);
+    readData();
+    printData();
+    delay(1000);
 }
 
 void initBME280() {
-  sensorBME280.setI2CAddress(0x76);
-  sensorBME280connected = sensorBME280.beginI2C();
+    sensorBME280.setI2CAddress(BME280_I2C);
+    sensorBME280connected = sensorBME280.beginI2C();
 
-  tft.print("BME280...   ");
+    display.print("BME280...   ");
 
-  if (sensorBME280connected) {
-    sensorBME280.setFilter(2); //0 to 4 is valid. Filter coefficient. See 3.4.4
-    sensorBME280.setStandbyTime(0); //0 to 7 valid. Time between readings. See table 27.
+    if (sensorBME280connected) {
+        sensorBME280.setFilter(2); //0 to 4 is valid. Filter coefficient. See 3.4.4
+        sensorBME280.setStandbyTime(0); //0 to 7 valid. Time between readings. See table 27.
 
-    sensorBME280.setTempOverSample(5); //0 .. 5 sets 0 to 16. 0 disables temp sensing. See table 24.
-    sensorBME280.setPressureOverSample(5); //0 .. 5 sets 0 to 16. 0 disables pressure sensing. See table 23.
-    sensorBME280.setHumidityOverSample(5); //0 .. 5 sets 0 to 16. 0 disables humidity sensing. See table 19.    
-   
-    sensorBME280.setMode(MODE_SLEEP); // MODE_NORMAL
+        sensorBME280.setTempOverSample(5); //0 .. 5 sets 0 to 16. 0 disables temp sensing. See table 24.
+        sensorBME280.setPressureOverSample(5); //0 .. 5 sets 0 to 16. 0 disables pressure sensing. See table 23.
+        sensorBME280.setHumidityOverSample(5); //0 .. 5 sets 0 to 16. 0 disables humidity sensing. See table 19.
 
-    int16_t backupX = tft.getCursorX();
-    int16_t backupY = tft.getCursorY();
-    for(int i=1; i<=100; i++){
-      readData();
-      if ((i % 5) == 0) {
-          tft.setCursor(backupX, backupY);
-          tft.print(i);
-      }
-      delay(20);
+        sensorBME280.setMode(MODE_SLEEP); // MODE_NORMAL
+
+        int16_t backupX = display.getCursorX();
+        int16_t backupY = display.getCursorY();
+        for(int i=1; i<=100; i++) {
+            readData();
+            if ((i % 5) == 0) {
+                display.setCursor(backupX, backupY);
+                display.print(i);
+            }
+            delay(20);
+        }
+        display.setCursor(backupX, backupY);
+        display.setTextColor(ST7735_GREEN, ST7735_BLACK);
+        display.println("[OK]");
+    } else {
+        display.setTextColor(ST7735_RED, ST7735_BLACK);
+        display.println("[Error]");
     }
-    tft.setCursor(backupX, backupY);
-    tft.setTextColor(ST7735_GREEN, ST7735_BLACK);
-    tft.println("[OK]");    
-  } else {
-    tft.setTextColor(ST7735_RED, ST7735_BLACK);
-    tft.println("[Error]");
-  }
+}
+
+void initCCS811() {
+    CCS811Core::status returnCode = sensorCCS811.begin();
+    if (returnCode == CCS811Core::SENSOR_SUCCESS) {
+        sensorCCS811connected = true;
+    }
+
+    display.print("CCS811...   ");
+
+    if (sensorCCS811connected) {
+        display.setTextColor(ST7735_GREEN, ST7735_BLACK);
+        display.println("[OK]");
+    } else {
+        display.setTextColor(ST7735_RED, ST7735_BLACK);
+        display.println("[Error]");
+    }
 }
 
 void readData() {
-  if (sensorBME280connected) {
-    sensorBME280.setMode(MODE_FORCED);
-    while(sensorBME280.isMeasuring() == false) ; //Wait for sensor to start measurment
-    while(sensorBME280.isMeasuring() == true) ; //Hang out while sensor completes the reading
-    
-    dataTemperature = sensorBME280.readTempC();
-    dataHumidity = sensorBME280.readFloatHumidity();
-    dataPressure = sensorBME280.readFloatPressure();    
-    
-    sensorBME280.setMode(MODE_SLEEP);
-  }
+    if (sensorBME280connected) {
+        sensorBME280.setMode(MODE_FORCED);
+        while(sensorBME280.isMeasuring() == false) ; //Wait for sensor to start measurment
+        while(sensorBME280.isMeasuring() == true) ; //Hang out while sensor completes the reading
+
+        dataTemperature = sensorBME280.readTempC();
+        dataHumidity = sensorBME280.readFloatHumidity();
+        dataPressure = sensorBME280.readFloatPressure();
+
+        sensorBME280.setMode(MODE_SLEEP);
+    }
 }
 
 void printData() {
-  if (!sensorBME280connected) {
-    return;
-  }
-  
-  char dataBuf[16];  
-  char tempString[10];
-  char tempStringTwo[10];
-  tft.setTextSize(2);
+    if (!sensorBME280connected) {
+        return;
+    }
 
-  
-
-  dtostrf(dataTemperature, 5, 2, tempString);
-  sprintf(dataBuf, "%s%cC", tempString, 247);
-  
-  tft.setCursor(4, 8);
-  tft.setTextColor(WeatherCalculations::mapTemperatureColor(dataTemperature), ST7735_BLACK);
-  tft.print(dataBuf);
+    char dataBuf[16];
+    char tempString[10];
+    char tempStringTwo[10];
+    display.setTextSize(2);
 
 
 
-  float heatIndex = WeatherCalculations::getHeatIndex(dataTemperature, dataHumidity);
-  dtostrf(heatIndex, 5, 2, tempString);
-  sprintf(dataBuf, "%s%cC    HI", tempString, 247);
-  
-  tft.setCursor(4, 32);
-  tft.setTextColor(WeatherCalculations::mapTemperatureColor(heatIndex), ST7735_BLACK);
-  tft.print(dataBuf);
+    dtostrf(dataTemperature, 5, 2, tempString);
+    sprintf(dataBuf, "%s%cC", tempString, 247);
+
+    display.setCursor(4, 8);
+    display.setTextColor(WeatherCalculations::mapTemperatureColor(dataTemperature), ST7735_BLACK);
+    display.print(dataBuf);
 
 
 
-  float dewPoint = WeatherCalculations::getDewPoint(dataTemperature, dataHumidity);
-  dtostrf(dewPoint, 5, 2, tempString);
-  sprintf(dataBuf, "%s%cC   DEW", tempString, 247);
-  
-  tft.setCursor(4, 56);
-  tft.setTextColor(WeatherCalculations::mapDewPointColor(dewPoint), ST7735_BLACK);
-  tft.print(dataBuf); 
+    float heatIndex = WeatherCalculations::getHeatIndex(dataTemperature, dataHumidity);
+    dtostrf(heatIndex, 5, 2, tempString);
+    sprintf(dataBuf, "%s%cC    HI", tempString, 247);
 
-  
-
-  float absoluteHumidity = WeatherCalculations::getAbsoluteHumidity(dataTemperature, dataHumidity);
-  dtostrf(dataHumidity, 5, 2, tempString);
-  dtostrf(absoluteHumidity, 2, 0, tempStringTwo);
-  sprintf(dataBuf, "%s%% %s%%  H", tempString, tempStringTwo);
-  
-  tft.setCursor(4, 80);
-  tft.setTextColor(ST7735_GREEN, ST7735_BLACK);
-  tft.print(dataBuf);
+    display.setCursor(4, 32);
+    display.setTextColor(WeatherCalculations::mapTemperatureColor(heatIndex), ST7735_BLACK);
+    display.print(dataBuf);
 
 
 
-  dtostrf(dataPressure / 100.0F, 7, 2, tempString);
-  sprintf(dataBuf, "%s   hPa", tempString);  
-  
-  tft.setCursor(4, 104);
-  tft.setTextColor(ST7735_GREEN, ST7735_BLACK);
-  tft.print(dataBuf);
+    float dewPoint = WeatherCalculations::getDewPoint(dataTemperature, dataHumidity);
+    dtostrf(dewPoint, 5, 2, tempString);
+    sprintf(dataBuf, "%s%cC   DEW", tempString, 247);
+
+    display.setCursor(4, 56);
+    display.setTextColor(WeatherCalculations::mapDewPointColor(dewPoint), ST7735_BLACK);
+    display.print(dataBuf);
+
+
+
+    float absoluteHumidity = WeatherCalculations::getAbsoluteHumidity(dataTemperature, dataHumidity);
+    dtostrf(dataHumidity, 5, 2, tempString);
+    dtostrf(absoluteHumidity, 2, 0, tempStringTwo);
+    sprintf(dataBuf, "%s%% %s%%  H", tempString, tempStringTwo);
+
+    display.setCursor(4, 80);
+    display.setTextColor(ST7735_GREEN, ST7735_BLACK);
+    display.print(dataBuf);
+
+
+
+    dtostrf(dataPressure / 100.0F, 7, 2, tempString);
+    sprintf(dataBuf, "%s   hPa", tempString);
+
+    display.setCursor(4, 104);
+    display.setTextColor(ST7735_GREEN, ST7735_BLACK);
+    display.print(dataBuf);
 }
